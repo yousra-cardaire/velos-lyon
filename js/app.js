f886428527710c40287f2fd942c697608d524ec1
yousra-cardaire <76117507+yousra-cardaire@users.noreply.github.com>
2021-06-07 08:33:30 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
-    // Initialisation de la latitude et de la longitude de la carte
+// Initialisation de la latitude et de la longitude de la carte
 let lat = 45.7578;
 let lon = 4.8351;
-let lyonMap = null;
+let map = null;
 
-// Iinitialisation de la carte
+// Initialisation de la carte
 function initMap() {
-    // Création de l'objet "lyonMap" et insération dans l'élément HTML qui a l'ID "map"
-    lyonMap = L.map('map').setView([lat, lon], 11);
+    // Création de l'objet "map" que l'on insère dans la div avec l'ID "map"
+    map = L.map('map').setView([lat, lon], 12);
     // Cartes non récupérées (tiles) par défaut par leaflet. On précise le lieu de récupération. Ici, openstreetmap.fr
     L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
         attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
         minZoom: 1,
         maxZoom: 20
-    }).addTo(lyonMap);
+    }).addTo(map);
 
 }
 
@@ -41,6 +41,10 @@ window.onload = function(){
         console.log("erreur de connexion");
     
     });
+
+    function Reserver(){
+          console.log("réservation");
+        }
     
     xhr.send(null);
 
@@ -48,25 +52,23 @@ window.onload = function(){
     response = JSON.parse(response);
     response.forEach(function (info) {
 
-
         var info_station =                                                                        //Récupération des différentes infos que l'on va placer dans le popup
         '<div>'  +
         '<ul>' +
         '<li>Station : ' + info.address + '</li>' +
         '<li>Nombre de vélos : ' + info.bike_stands + '</li>' +
-        '<li>Nombre de vélos disponibles : ' + info.available_bikes + '</li>' +
-        '<li>Statut de la station : ' + info.status + '</li>' +
+        '<li>Vélos disponibles : ' + info.available_bikes + '</li>' +
         '</ul>' +
-        '<button type="button" class="btn btn-primary onclick="Function()">Réserver</button>' + 
+        '<button type="button" class="btn" onclick="Reserver()">Réserver</button>' + 
         '</div>'
-
-
+        
+        
         L.marker(                                                                                  //Ajout des marqueurs
             [info.position.lat, info.position.lng],
             {
             "jcdecauxInfo": info}           // on stocke ici toutes les infos
         ) 
-        .addTo(lyonMap)                     // fonction d'appel 
+        .addTo(map)                     // fonction d'appel 
         .bindPopup(info_station);
     });
 }
